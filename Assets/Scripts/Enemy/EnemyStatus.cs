189396419d28c7bca1f4c5127975fcae98a3f6ac
kdh5790 <k_0106@naver.com>
2025-03-10 23:24:50 +0900

    [ Chore ] 보스 피격 시 메쉬 색상 변경되도록 추가

--- Assets/Scripts/Enemy/EnemyStatus.cs
+++ Assets/Scripts/Enemy/EnemyStatus.cs
@@ -8,11 +8,14 @@ public class EnemyStatus : MonoBehaviour, IDamageable
 
     private bool isInvicibility = false;
     private bool isDie = false;
+
     private Animator animator;
+    private SkinnedMeshRenderer[] meshRenderers;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        meshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
     }
 
     public void OnDamaged(int damage)
@@ -26,6 +29,7 @@ public class EnemyStatus : MonoBehaviour, IDamageable
             Die();
         }
 
+        StartCoroutine(DamageFlash());
         StartCoroutine(InvicibilityCoroutine(0.5f));
     }
 
@@ -36,6 +40,21 @@ public class EnemyStatus : MonoBehaviour, IDamageable
         yield return new WaitForSeconds(duration);
 
         isInvicibility = false;
+    }
+
+    IEnumerator DamageFlash()
+    {
+        for (int i = 0; i < meshRenderers.Length; i++)
+        {
+            meshRenderers[i].material.color = new Color(1, 0.6f, 0.6f);
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        for (int i = 0; i < meshRenderers.Length; i++)
+        {
+            meshRenderers[i].material.color = Color.white;
+        }
     }
 
     private void Die()
