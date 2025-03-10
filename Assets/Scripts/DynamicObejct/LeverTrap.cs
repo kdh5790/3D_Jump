71e2ead7d87b29dc9160b6e18ba489e04ef87f68
kdh5790 <k_0106@naver.com>
2025-03-10 19:18:23 +0900

    [ Feat ] 보스전에서 사용 할 레버 함정 추가

---
+++ Assets/Scripts/DynamicObejct/LeverTrap.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrap : MonoBehaviour, ILeverActionable
+{
+    [SerializeField] private Transform trapTransform;
+    [SerializeField] private Lever lever;
+    public Lever Lever => lever;
+
+    private BoxCollider boxCollider;
+
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.enabled = false;
+    }
+
+    public void EndLeverAction()
+    {
+        lever.InitLeverRotation();
+        
+    }
+
+    public void StartLeverAction()
+    {
+        StartCoroutine(ActiveTrap());
+    }
+
+    private IEnumerator ActiveTrap()
+    {
+        boxCollider.enabled = true;
+        Vector3 startScale = trapTransform.localScale;
+        Vector3 targetScale = new Vector3(1, 1, 1);
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+            trapTransform.localScale = Vector3.Lerp(startScale, targetScale, t);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+            trapTransform.localScale = Vector3.Lerp(targetScale, startScale, t);
+            yield return null;
+        }
+
+        boxCollider.enabled = false;
+
+        EndLeverAction();
+    }
+}
--- Assets/Scripts/InteractiveObject/Lever.cs
+++ Assets/Scripts/InteractiveObject/Lever.cs
@@ -5,8 +5,8 @@ public class Lever : MonoBehaviour, IInteractive
 {
     [SerializeField] private Transform leverHandle;
     [SerializeField] private ObjectInfo info;
-    
-    private ILeverActionable leverActionObject;
+
+    private ILeverActionable[] leverActionObject;
     private Vector3 firstRot = new Vector3(-40f, 0, 0);
     private Vector3 lastRot = new Vector3(90f, 0, 0);
 
@@ -15,22 +15,32 @@ public class Lever : MonoBehaviour, IInteractive
 
     private void Start()
     {
-        leverActionObject = GetComponentInParent<ILeverActionable>();
+        leverActionObject = GetComponentsInParent<ILeverActionable>();
+
+        if(leverActionObject.Length == 0)
+            leverActionObject = GetComponentsInChildren<ILeverActionable>();
+
+        if (leverActionObject.Length == 0)
+            Debug.LogError("레버와 상호작용 할 오브젝트를 찾지 못했습니다. 설정을 확인해주세요.");
+
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F) && canInteract && !isLeverAction)
+        if (Input.GetKeyDown(KeyCode.F) && canInteract && !isLeverAction)
         {
             StartCoroutine(RotateLever(firstRot, lastRot));
-            leverActionObject.StartLeverAction();
+
+            foreach (var lever in leverActionObject)
+                lever.StartLeverAction();
+
             isLeverAction = true;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             canInteract = true;
         }
